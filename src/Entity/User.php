a99ea347ac8874c9cb81833de6cd11febab79171
Mighri Houssem <mighri.houssem.official@gmail.com>
2022-11-22 20:24:40 +0100

    Security Remember me login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,7 +44,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->id;
+        return (string) $this->email;
     }
 
     /**
